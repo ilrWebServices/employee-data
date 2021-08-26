d0f861487b62b53e4b340219fadc3cdc01c48f52
Jeff Amaral <jeff.amaral@cornell.edu>
2021-08-26 12:32:03 -0400

    Move settings file into main script and use environment variables for sensitive data

--- pull_people_data.php
+++ pull_people_data.php
@@ -6,9 +6,17 @@
  * @todo Retrieve faculty leave from a Box file rather than storing it here which requires a redeployment when it changes
  *
  */
-
-require 'ilr-faculty-data-conf.php';
-
+define('SETTINGS', [
+  'ai_api_url' => 'https://webservices.digitalmeasures.com/login/service/v4',
+  'ai_userid' => getenv('AI_USER'),
+  'ai_pwd' => getenv('AI_PASS'),
+  'ldap_start' => 'ou=people,o=cornell university,c=us',
+  'ldap_filter' => '(|(uid=cl672)(uid=mlc13)(uid=ldd3)(uid=mb2693)(uid=lrt4)(uid=sdm39)(uid=dca58)(uid=ajc22)(uid=kfh7)(uid=vmb2)(uid=hck2)(uid=cjm267)(uid=rss14)(uid=mfl55)(uid=bdd28)(cornelledudeptname1=LIBR - Catherwood*)(&(|(cornelledudeptname1=LIBR - Hospitality, Labor*)(cornelledudeptname1=LIBR - Management Library)(cornelledudeptname1=LIBR - ILR Catherwood Library))(cornelleducampusaddress=Ives Hall*))(cornelledudeptname1=IL-*)(cornelledudeptname1=E-*)(cornelledudeptname1=ILR*)(cornelledudeptname1=CAHRS))',
+  'ldap_server' => 'directory.cornell.edu',
+  'ldap_port' => '389',
+  'ldap_user' => getenv('LDAP_USER'),
+  'ldap_pwd' => getenv('LDAP_PASS'),
+]);
 
 $ldap_attributes = array(
   'displayname',
