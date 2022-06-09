1c58a705d4c4601fd338cd77faa2565749d69f7f
David DeMello <daviddemello@gmail.com>
2022-06-09 14:13:38 -0400

    Adds Jennifer Dean to the LDAP filter

--- pull_people_data.php
+++ pull_people_data.php
@@ -11,7 +11,7 @@ define('SETTINGS', [
   'ai_userid' => getenv('AI_USER'),
   'ai_pwd' => getenv('AI_PASS'),
   'ldap_start' => 'ou=people,o=cornell university,c=us',
-  'ldap_filter' => '(|(uid=pl82)(uid=ad838)(uid=cl672)(uid=mlc13)(uid=ldd3)(uid=mb2693)(uid=lrt4)(uid=sdm39)(uid=dca58)(uid=ajc22)(uid=kfh7)(uid=vmb2)(uid=hck2)(uid=cjm267)(uid=rss14)(uid=mfl55)(uid=bdd28)(cornelledudeptname1=LIBR - Catherwood*)(&(|(cornelledudeptname1=LIBR - Hospitality, Labor*)(cornelledudeptname1=LIBR - Management Library)(cornelledudeptname1=LIBR - ILR Catherwood Library))(cornelleducampusaddress=Ives Hall*))(cornelledudeptname1=IL-*)(cornelledudeptname1=E-*)(cornelledudeptname1=ILR*)(cornelledudeptname1=CAHRS))',
+  'ldap_filter' => '(|(uid=jds13)(uid=pl82)(uid=ad838)(uid=cl672)(uid=mlc13)(uid=ldd3)(uid=mb2693)(uid=lrt4)(uid=sdm39)(uid=dca58)(uid=ajc22)(uid=kfh7)(uid=vmb2)(uid=hck2)(uid=cjm267)(uid=rss14)(uid=mfl55)(uid=bdd28)(cornelledudeptname1=LIBR - Catherwood*)(&(|(cornelledudeptname1=LIBR - Hospitality, Labor*)(cornelledudeptname1=LIBR - Management Library)(cornelledudeptname1=LIBR - ILR Catherwood Library))(cornelleducampusaddress=Ives Hall*))(cornelledudeptname1=IL-*)(cornelledudeptname1=E-*)(cornelledudeptname1=ILR*)(cornelledudeptname1=CAHRS))',
   'ldap_server' => 'directory.cornell.edu',
   'ldap_port' => '389',
   'ldap_user' => getenv('LDAP_USER'),
