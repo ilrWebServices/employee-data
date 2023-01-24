43ece62706e7e3079566ded8a9e57ce96e014301
David DeMello <daviddemello@gmail.com>
2023-01-24 15:55:53 -0500

    Adds "Research Professor" as a recognized title for faculty who get profiles.

--- pull_people_data.php
+++ pull_people_data.php
@@ -236,6 +236,7 @@ function ldap2xml($ldap) {
     $faculty_titles[] = 'Research Associate Sr';
     $faculty_titles[] = 'Research Associate';
     $faculty_titles[] = 'Scholar Visit';
+    $faculty_titles[] = 'Research Professor';
 
     $temp_faculty = array('lac24', 'lha1', 'gc32', 'ljf8', 'lsg3', 'vmb2', 'zen2', 'srt82', 'so44', 'jds13','lrs95', 'elg234', 'jn497', 'lhc62', 'mjb62', 'cjb39', 'nah36', 'ak839', 'kal238', 'ko259', 'mcs378', 'ws283', 'jt693', 'aaw43', 'mfl55','no232','kfh7');
     $deans = array('ajc22', 'ljb239', 'jeg68', 'rss14','mlc13','ldd3','lrt4');
